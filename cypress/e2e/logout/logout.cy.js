76ca428892c6b45a80c91b17887d9ee0fc3ea36e
pegikkk <37980196+pegikkk@users.noreply.github.com>
2023-06-05 06:52:49 +0700

    TugasProjectAkhir-Veggy

--- cypress/e2e/logout/logout.cy.js
+++ cypress/e2e/logout/logout.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('LogOut', () => {
+  it('M001-8 - Berhasil Log Out', () => {
+    cy.login()
+    cy.get('.oxd-userdropdown-tab').click({force:true})
+    cy.get(':nth-child(4) > .oxd-userdropdown-link').click()
   })
 })
--- cypress/e2e/menuPIM/addEmployee.cy.js
+++ cypress/e2e/menuPIM/addEmployee.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Modul  PIM - Add Employee', () => {
+  it('M-011.1 ', () => {
+    cy.login()
+    cy.get(':nth-child(2) > .oxd-main-menu-item').click()
+    cy.get('.oxd-topbar-body-nav > ul > :nth-child(3)').click()
+    cy.get('.oxd-file-div > .oxd-icon-button > .oxd-icon').click()
+    cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type('Sharon')
+    cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type('Daaneva')
+    cy.get('.oxd-button--secondary').click({force:true})
   })
 })
